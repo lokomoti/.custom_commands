9706b058885bcde8719005827a4b88d0fab7de7d
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 10:01:33 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -12,11 +12,11 @@ sudo chmod +x ~/.custom_commands/run.sh
 sudo chmod +x ~/.custom_commands/*
 
 echo "Adding line to rc.local for startup"
-rc_line='source /home/pi/.custom_commands/run.sh &'
+rc_line='source /home/pi/.custom_commands/run.sh &\n'
 
-if grep -Fq $rc_line /etc/rc.local
+if grep -Fq "$rc_line" /etc/rc.local
 then echo "line already present in rc.local file"
-else echo "adding line"; sudo sed -i -e '$i $rc_line\n' /etc/rc.local
+else echo "adding line"; sudo sed -i -e "$i \$rc_line" /etc/rc.local
 fi
 
 #sudo sed -i '/source ~/.custom_commands/run.sh &/d' ./etc/rc.local
