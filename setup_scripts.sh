5d17d261fe167c3f751c8e2034e4c603cbcb0d7a
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 08:42:59 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -10,8 +10,10 @@ sudo chmod +x ~/.custom_commands/*
 source ~/.custom_commands/run.sh
 
 echo "Adding line to rc.local for startup"
+#sudo sed -i '/source ~/.custom_commands/run.sh &/d' ./etc/rc.local
+
 # Add line to rc.local to automaticaly load custom commands on startup
-sudo sed -i -e '$i \source ~/.custom_commands/run.sh &\n' rc.local
+sudo sed -i -e '$i sudo ~/.custom_commands/boot.sh &\n' /etc/rc.local
 
 clear
 
