01959ea7e5b0271490c5456d3a4f4b2331a8f690
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 08:56:08 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -1,6 +1,6 @@
 #sudo rm -r /boot/setup_scripts.sh 2>&1 >/dev/null 
-echo "If file already present - deleting file"
-sudo rm -r ~/.custom_commands/
+#echo "If file already present - deleting file"
+#sudo rm -r ~/.custom_commands/
 
 echo "Cloning repository"
 cd ~ && git clone https://github.com/lokomoti/.custom_commands.git 2>&1 >/dev/null 
