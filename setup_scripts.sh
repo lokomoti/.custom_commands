f66e330503060692f6d4697c9944d17c475ca65d
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 09:15:18 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -5,7 +5,7 @@ sudo rm -r ~/.custom_commands/
 
 echo "Cloning repository"
 cd ~
-git clone https://github.com/lokomoti/.custom_commands.git 2>&1 >/dev/null 
+git clone https://github.com/lokomoti/.custom_commands.git #2>&1 >/dev/null 
 
 echo "Making executable and sourcing commands"
 sudo chmod +x ~/.custom_commands/run.sh
@@ -13,21 +13,13 @@ sudo chmod +x ~/.custom_commands/*
 
 echo "Adding line to rc.local for startup"
 
-if 
-  grep -Fq "~/.custom_commands/boot.sh &" /etc/rc.local
-then 
-  echo "line already present in rc.local file"
-else 
-  echo "adding line"
-  # Add line to rc.local to automaticaly load custom commands on startup
-  sudo sed -i -e '$i sudo ~/.custom_commands/boot.sh &\n' /etc/rc.local
+if grep -Fq "~/.custom_commands/boot.sh &" /etc/rc.local
+then echo "line already present in rc.local file"
+else echo "adding line"; sudo sed -i -e '$i sudo ~/.custom_commands/boot.sh &\n' /etc/rc.local
 fi
 
 #sudo sed -i '/source ~/.custom_commands/run.sh &/d' ./etc/rc.local
 
 source /home/pi/.custom_commands/run.sh
 
-clear
-
-sudo upgrade_pi
-sudo remove_setup_file
+upgrade_pi
