699b4f2633cb61fc76e8434b1e76265412df945b
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 09:30:56 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
 
-function upgrade_pi()
-{
-  sudo ~/.custom_commands/upgrade_pi.sh
+function upgrade_pi() {
+  sudo ./upgrade_pi.sh
 }
 
-function remove_setup_file()
-{
+function remove_setup_file() {
   clear
   echo "removing setup file..."
   sudo rm -r ~/setup_scripts.sh
