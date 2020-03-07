85c92c557e1d9b97366c2c08ad94ea355f7a69fb
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 09:08:47 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -6,14 +6,24 @@ echo "Cloning repository"
 cd ~ && git clone https://github.com/lokomoti/.custom_commands.git 2>&1 >/dev/null 
 
 echo "Making executable and sourcing commands"
+sudo chmod +x ~/.custom_commands/run.sh
 sudo chmod +x ~/.custom_commands/*
-source ~/.custom_commands/run.sh
 
 echo "Adding line to rc.local for startup"
+
+if 
+  grep -Fq "~/.custom_commands/boot.sh &" /etc/rc.local
+then 
+  echo "line already present in rc.local file"
+else 
+  echo "adding line"
+  # Add line to rc.local to automaticaly load custom commands on startup
+  sudo sed -i -e '$i sudo ~/.custom_commands/boot.sh &\n' /etc/rc.local
+fi
+
 #sudo sed -i '/source ~/.custom_commands/run.sh &/d' ./etc/rc.local
 
-# Add line to rc.local to automaticaly load custom commands on startup
-sudo sed -i -e '$i sudo ~/.custom_commands/boot.sh &\n' /etc/rc.local
+source /home/pi/.custom_commands/run.sh
 
 clear
 
