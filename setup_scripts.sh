c9197d6283a18ffd44e6a04c2ec7b0506454677f
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 07:12:04 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -1,14 +1,18 @@
 #sudo rm -r /boot/setup_scripts.sh 2>&1 >/dev/null 
-echo "setting up files"
+echo "If file already present - deleting file"
 sudo rm -r ~/.custom_commands/
 
+echo "Cloning repository"
 cd ~ && git clone https://github.com/lokomoti/.custom_commands.git 2>&1 >/dev/null 
 
+echo "Making executable and sourcing commands"
 sudo chmod +x ~/.custom_commands/*
 source ~/.custom_commands/run.sh
 
+echo "Adding line to rc.local for startup"
 # Add line to rc.local to automaticaly load custom commands on startup
 sed -e '$i \source ~/.custom_commands/run.sh &\n' rc.local
 
+clear
 
 ~/.custom_commands/upgrade_pi.sh
