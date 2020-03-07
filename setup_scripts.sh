f9d6b766b6e8559d498a36d7035f9a93cae5320f
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-07 06:57:47 +0100

    Update setup_scripts.sh

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -1,5 +1,6 @@
+sudo rm -r /boot/setup_scripts.sh 2>&1 >/dev/null 
 echo "setting up files"
 cd ~ && git clone https://github.com/lokomoti/.custom_commands.git 2>&1 >/dev/null 
-sudo chmod +x ~/.custom_commands/upgrade_pi.sh 
+sudo chmod +x ~/.custom_commands/*
 
-~/.custom_commands/upgrade_pi.sh
+~/.custom_commands/run.sh
