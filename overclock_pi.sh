821f0fdc969e51b872a655413e0b499df282dd87
lokomoti <40903296+lokomoti@users.noreply.github.com>
2020-03-18 05:35:06 +0100

    Update overclock_pi.sh

--- overclock_pi.sh
+++ overclock_pi.sh
@@ -4,6 +4,10 @@ clear
 echo "Overclock script for rpi, enter your type:"
 
 
-# Pass rpi type:
+### Pass rpi type:
+# pi zero response
+"Raspberry Pi Zero W Rev 1.1"
 
 
+
+cat /proc/device-tree/model
